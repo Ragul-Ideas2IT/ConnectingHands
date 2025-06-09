71dc40b7fbc7162657cb897d9066e8fe692e62c0
Ragul Venkatesan <ragul.venkatesan@ideas2it.com>
2025-06-09 19:56:31 +0530

    feat: Implement API documentation (BE-012) - Add OpenAPI/Swagger documentation with SpringDoc

---
+++ src/main/java/com/connectinghands/config/OpenApiConfig.java
@@ -0,0 +1,39 @@
+package com.connectinghands.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Connecting Hands API")
+                        .description("API documentation for the Connecting Hands platform - connecting orphanages with donors")
+                        .version("1.0.0")
+                        .contact(new Contact()
+                                .name("Connecting Hands Team")
+                                .email("support@connectinghands.org")
+                                .url("https://connectinghands.org"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("https://www.apache.org/licenses/LICENSE-2.0")))
+                .addSecurityItem(new SecurityRequirement().addList("JWT"))
+                .components(new Components()
+                        .addSecuritySchemes("JWT", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                                .in(SecurityScheme.In.HEADER)
+                                .name("Authorization")));
+    }
+} 
